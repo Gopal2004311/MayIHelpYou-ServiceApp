30bfa86213b80d501d94313e847ca3d0a5ed4d03
gopal2004311 <gopalsadavarte555@gmail.com>
2024-04-15 21:49:42 +0530

    Final Commit to confirm all files

---
+++ admin/_adminProfile.php
@@ -0,0 +1,34 @@
+<h1 class="heading">Dashboard</h1>
+                    <div class="box-con">
+                        <div class="box" id="box1">
+                            <div class="text">
+                                <h2>Likes</h2>
+                                <i class="fa-solid fa-thumbs-up"></i>
+                            </div>
+                            <div class="numbers">3,454</div>
+                        </div>
+                        <div class="box" id="box2">
+                            <div class="text">
+                                <h2>Subscribers</h2>
+                                <i class="fa-brands fa-youtube"></i>
+                            </div>
+                            <div class="numbers">3,43,454</div>
+                        </div>
+                        <div class="box" id="box3">
+                            <div class="text">
+                                <h2>Followers</h2>
+                                <i class="fa-solid fa-people-group"></i>
+                            </div>
+                            <div class="numbers">1,00,454</div>
+                        </div>
+                        <div class="box" id="box4">
+                            <div class="text">
+                                <h2>Messages</h2>
+                                <i class="fa-solid fa-comment"></i>
+                            </div>
+                            <div class="numbers">355</div>
+                        </div>
+                    </div>
+                    <div class="info-con">
+
+                    </div>
